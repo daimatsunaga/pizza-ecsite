0a5de16a23e0b79432163f98cd7ff7bd51334194
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2021-11-24 22:58:59 +0900

    loginコントローラーの作成（login,logoutメソッドを作成））

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,7 +15,6 @@ import com.example.service.UserService;
 
 
 @Controller
-@RequestMapping("")
 public class LoginController {
 	@Autowired
 	private HttpSession session;
@@ -34,13 +34,20 @@ public class LoginController {
 	}
 	//ログインを行う
 	@RequestMapping("/login")
-	public String login(LoginForm form) {
+	public String login(LoginForm form, Model model) {
 		User user = userService.findByPasswordAndMailAddress(form.getEmail(), form.getPassword());
 		
 		if(user == null) {
+			model.addAttribute("loginError", "メールアドレス、またはパスワードが間違っています");
 			return "login";
 		}
 		session.setAttribute("user", user);
+		return "item_list_pizza";
+	}
+	//ログアウトを行う
+	@RequestMapping("/logout")
+	public String login() {
+		session.invalidate();
 		return "item_list_pizza";
 	}
 	
